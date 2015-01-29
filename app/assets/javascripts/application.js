9cfe782692ad3047588962b5e36c2ebd5c6cfec0
mrjadaml <mrjadaml@gmail.com>
2015-01-28 21:03:37 -0700

    jqueryness

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,18 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+var humanDist = $('.human-dist').text()
+var zombieDist = $('.zombie-dist').text()
+console.log(humanDist)
+console.log(zombieDist)
+
+var pow = function() {
+  if humanDist > zombieDist
+    console.log('w00t!')
+  else
+    console.log('xombie z00t!')
+};
+
+pow();
